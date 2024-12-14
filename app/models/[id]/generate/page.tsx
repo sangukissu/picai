35419a26378d8eaa304e22123b8ff1e9398bafb3
sazeai <the6thsense137@gmail.com>
2024-12-14 20:40:14 +0530

    Fix type error in generate image page by updating prop types

--- app/models/[id]/generate/page.tsx
+++ app/models/[id]/generate/page.tsx
@@ -1,8 +1,13 @@
 import { Metadata } from 'next'
 import { GenerateImageClient } from './generate-image-client'
 
-type GenerateImageProps = {
-  params: { id: string };
+type Params = {
+  id: string;
+}
+
+type Props = {
+  params: Params;
+  searchParams: { [key: string]: string | string[] | undefined };
 }
 
 export const metadata: Metadata = {
@@ -10,7 +15,7 @@ export const metadata: Metadata = {
   description: 'Generate an image using your trained model',
 }
 
-export default function GenerateImage({ params }: GenerateImageProps) {
+export default function GenerateImage({ params }: Props) {
   return <GenerateImageClient id={params.id} />
 }
 
