cdfc4c4c3e61e48c555f012878fb82b4e6a3b0aa
sazeai <the6thsense137@gmail.com>
2024-12-14 20:45:10 +0530

    Update generate image page to use correct Next.js 13+ types

--- app/models/[id]/generate/page.tsx
+++ app/models/[id]/generate/page.tsx
@@ -1,21 +1,16 @@
 import { Metadata } from 'next'
 import { GenerateImageClient } from './generate-image-client'
 
-type Params = {
-  id: string;
-}
-
-type Props = {
-  params: Params;
-  searchParams: { [key: string]: string | string[] | undefined };
-}
-
 export const metadata: Metadata = {
   title: 'Generate Image',
   description: 'Generate an image using your trained model',
 }
 
-export default function GenerateImage({ params }: Props) {
+export default function GenerateImage({
+  params,
+}: {
+  params: { id: string }
+}) {
   return <GenerateImageClient id={params.id} />
 }
 
